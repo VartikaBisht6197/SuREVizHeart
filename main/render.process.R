5fe1c83997ac1dca0877ae888a7c27238715ccae
vartikabisht6197 <vartika@annogen.bio>
2024-11-26 10:10:31 +0100

    Re-define logic for plotting user defined inputs. Checking, initializing and recognising data type done in seperate script. Here, only plot.

--- main/render.process.R
+++ main/render.process.R
@@ -29,37 +29,6 @@
 #
 #-----------------------------------------------------------------------------------------------
 
-########################
-########################
-# User Defined BW+BEDs #
-########################
-########################
-
-# Initialize variables for user-defined files
-user_defined <- NULL
-user_defined_bw <- NULL
-user_defined_bed <- NULL
-
-# Populate the user_defined list with selected files
-user_defined <- as.list(selected_files())
-if (length(user_defined) > 0) {
-    names(user_defined) <- lapply(user_defined, basename)
-
-    # Split the list based on file extensions
-    user_defined_bw <- user_defined[grepl("\\.bw$", user_defined)]
-    user_defined_bw <- user_defined[grepl("\\.bigwig$", user_defined)]
-    user_defined_bw <- user_defined[grepl("\\.bigWig$", user_defined)]
-    user_defined_bed <- user_defined[grepl("\\.bed$", user_defined)]
-
-    # Check if the lists are empty and set them to NULL if they are
-    if (length(user_defined_bw) == 0) {
-        user_defined_bw <- NULL
-    }
-    if (length(user_defined_bed) == 0) {
-        user_defined_bed <- NULL
-    }
-}
-
 # Rendering after knowing chr pos pos1 pos2
 #############
 # SuRE plot #
@@ -67,7 +36,7 @@ if (length(user_defined) > 0) {
 
 # Source the script for SuRE plot generation
 source(file.path(appDIR, "modules", "plot", "sureplot.R"), local = TRUE )
-log.this.info("SuRE Plot : Plotted ✅")
+message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "SuRE Plot : Plotted ✅"))
 
 ##############
 # Gene plots #
@@ -75,7 +44,7 @@ log.this.info("SuRE Plot : Plotted ✅")
 
 # Source the script for gene plots and plotly visualizations
 source(file.path(appDIR, "modules", "plot", "geneplot.R"), local = TRUE )
-log.this.info("Gene Plot : Plotted ✅")
+message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "Gene Plot : Plotted ✅"))
 
 ###################
 # Gene expression #
@@ -85,7 +54,7 @@ log.this.info("Gene Plot : Plotted ✅")
 source(file.path(appDIR, "modules", "plot", "gene.expression.R"), local = TRUE )
 genes <- unique(genedata$GENE)
 gene.expression.plots <- get.expression.plot(genes)
-log.this.info("Gene Expression Plot : Plotted ✅")
+message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "Gene Expression Plot : Plotted ✅"))
 
 ###############
 # Bigwig plot #
@@ -104,10 +73,19 @@ bigwig.plot <- wrap_plots(
     gene.plot,
     SuREbigwigplots,
     plot.bigwigs(chr, pos1, pos2, pos, AC16ATACbw, "#645200", "common"),
-    ncol = 1, heights = c(0.2, 0.1, 0.7, 0.1)
+    ncol = 1, heights = c(0.3, 0.2, 0.7, 0.1)
 )
-log.this.info("SuRE Bigwig Tracks : Plotted SuRE bigwig and AC16 ATACseq plot ✅")
+message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "SuRE Bigwig Tracks : Plotted SuRE bigwig and AC16 ATACseq plot ✅"))
 
+###################################
+# User Defined MPRA data #
+###################################
+# Source the script for MPRA plot generation
+source(file.path(appDIR, "modules", "plot", "mpra.plot.R"), local = TRUE)
+user_defined_tsv_plots <- NULL
+for (tsv_file in names(user_defined_tsv)) {
+    user_defined_tsv_plots[[tools::file_path_sans_ext(basename(user_defined_tsv[[tsv_file]]))]] <- plot_tsv_file(user_defined_tsv[[tsv_file]],tsv_file, chr, pos1, pos2,pos)
+}
 
 ############################
 # User defined Bigwig plot #
@@ -118,9 +96,111 @@ source(file.path(appDIR, "modules", "plot", "bedplot.R"))
 SuREbigwig <<- FALSE
 # Initialize the plot for user-defined BigWig and BED files
 user.defined.plot <- NULL
+
+n_user_defined_tsv <- length(user_defined_tsv_plots)
+n_user_defined_bed <- length(user_defined_bed)
+n_user_defined_bw <- length(user_defined_bw)
+user_define_plots <- 2 + n_user_defined_tsv + n_user_defined_bed + n_user_defined_bw
+
+if (!is.null(user_defined_bw)) {
+    if (!is.null(user_defined_bed)) {
+        if(!is.null(user_defined_tsv_plots)){
+            # bigwig = 1 , bed = 1 , tsv = 1
+            user.defined.plot <- wrap_plots(
+                c(user_defined_tsv_plots, list(
+                plot.bigwigs(chr, pos1, pos2, pos, user_defined_bw, "#ff0084b3", "common"),
+                plot.beds(chr, pos1, pos2, pos, user_defined_bed),
+                static.snp.plot,
+                gene.plot)),
+                ncol = 1, heights = c(rep(0.2,n_user_defined_tsv), 0.2 * n_user_defined_bw, 0.1 * n_user_defined_bed, 0.2, 0.1)
+            )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined MPRA, bigwig, bed plots ✅"))
+
+        } else {
+            # bigwig = 1 , bed = 1 , tsv = 0
+            user.defined.plot <- wrap_plots(
+                plot.bigwigs(chr, pos1, pos2, pos, user_defined_bw, "#ff0084b3", "common"),
+                plot.beds(chr, pos1, pos2, pos, user_defined_bed),
+                static.snp.plot,
+                gene.plot,
+                ncol = 1, heights = c(0.2 * n_user_defined_bw, 0.1 * n_user_defined_bed, 0.2, 0.1)
+            )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined bigwig and bed plots ✅"))
+
+        }
+    } else {
+        if(!is.null(user_defined_tsv_plots)){
+            # bigwig = 1 , bed = 0 , tsv = 1
+            user.defined.plot <- wrap_plots(
+                c(user_defined_tsv_plots, list(
+                plot.bigwigs(chr, pos1, pos2, pos, user_defined_bw, "#ff0084b3", "common"),
+                static.snp.plot,
+                gene.plot)),
+                ncol = 1, heights = c(rep(0.2,n_user_defined_tsv), 0.2 * n_user_defined_bw, 0.2, 0.1)
+            )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined bigwig and MPRA plots ✅"))
+
+
+        } else {
+            # bigwig = 1 , bed = 0 , tsv = 0
+            user.defined.plot <- wrap_plots(
+                plot.bigwigs(chr, pos1, pos2, pos, user_defined_bw, "#ff0084b3", "common"),
+                static.snp.plot,
+                gene.plot,
+                ncol = 1, heights = c(0.2 * n_user_defined_bw, 0.2, 0.1)
+            )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots: Plotted user defined bigwig plots ✅"))
+        }
+    }
+} else {
+    if (!is.null(user_defined_bed)) {
+        if(!is.null(user_defined_tsv_plots)){
+            # bigwig = 0 , bed = 1 , tsv = 1
+            user.defined.plot <- wrap_plots(
+                c(user_defined_tsv_plots, list(plot.beds(chr, pos1, pos2, pos, user_defined_bed),static.snp.plot, gene.plot)),
+                ncol = 1, heights = c(rep(0.2,n_user_defined_tsv), 0.1 * n_user_defined_bed, 0.2, 0.1)
+            )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined bed and MPRA plots ✅"))
+        } else {
+            # bigwig = 0 , bed = 1 , tsv = 0
+            n_user_defined_bed <- length(user_defined_bed)
+            user.defined.plot <- wrap_plots(
+                plot.beds(chr, pos1, pos2, pos, user_defined_bed),
+                static.snp.plot,
+                gene.plot,
+                ncol = 1, heights = c(0.1 * n_user_defined_bed, 0.3, 0.2)
+            )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined bed plots ✅")) 
+        }
+    } else {
+        if(!is.null(user_defined_tsv_plots)){
+            # bigwig = 0 , bed = 0 , tsv = 1
+            n_user_defined_tsv <- length(user_defined_tsv_plots)
+            user.defined.plot <- wrap_plots(
+                c(user_defined_tsv_plots, list(static.snp.plot, gene.plot)),
+                ncol = 1, heights = c(rep(0.2,n_user_defined_tsv), 0.2, 0.1)
+            )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined MPRA plots ✅"))
+        } else {
+            # bigwig = 0 , bed = 0 , tsv = 0
+            user.defined.plot <- ggplot() +
+                theme_minimal() +
+                annotate("text", x = 0.5, y = 0.5, label = "No bigwigs, bed or MPRA data files uploaded", size = 6, hjust = 0.5, vjust = 0.5) +
+                theme(
+                    axis.title = element_blank(),
+                    axis.text = element_blank(),
+                    axis.ticks = element_blank(),
+                    panel.grid = element_blank()
+                )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : User has not defined beds, bigwigs or MPRA data. Plotted an empty plot. ⚠️"))
+        }
+    }
+}
+
+
 if (!is.null(user_defined_bw)) {
     
-    log.this.info(paste0("User defined bigwigs to be plotted :\n", paste(user_defined_bw, collapse = "\n")))
+    message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", paste0("User defined bigwigs to be plotted :\n", paste(user_defined_bw, collapse = "\n"))))
     n_user_defined_bw <- length(user_defined_bw)
     user.defined.plot <- wrap_plots(
         plot.bigwigs(chr, pos1, pos2, pos, user_defined_bw, "#ff0084b3", "common"),
@@ -128,11 +208,11 @@ if (!is.null(user_defined_bw)) {
         gene.plot,
         ncol = 1, heights = c(0.2 * n_user_defined_bw, 0.2, 0.1)
     )
-    log.this.info("User Defined Plots: Plotted user defined bigwigs ✅")
+    message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots: Plotted user defined bigwigs ✅"))
     
     if (!is.null(user_defined_bed)) {
 
-        log.this.info(paste0("User defined bed to be plotted :\n", paste(user_defined_bed, collapse = "\n")))
+        message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", paste0("User defined bed to be plotted :\n", paste(user_defined_bed, collapse = "\n"))))
         n_user_defined_bed <- length(user_defined_bed)
         user.defined.plot <- wrap_plots(
             plot.bigwigs(chr, pos1, pos2, pos, user_defined_bw, "#ff0084b3", "common"),
@@ -141,13 +221,26 @@ if (!is.null(user_defined_bw)) {
             gene.plot,
             ncol = 1, heights = c(0.2 * n_user_defined_bw, 0.1 * n_user_defined_bed, 0.2, 0.1)
         )
-        log.this.info("User Defined Plots : Plotted user defined beds ✅")
+        message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined beds ✅"))
+
+        if(!is.null(user_defined_tsv_plots)){
+            n_user_defined_tsv <- length(user_defined_tsv_plots)
+            user.defined.plot <- wrap_plots(
+                c(user_defined_tsv_plots, list(
+                plot.bigwigs(chr, pos1, pos2, pos, user_defined_bw, "#ff0084b3", "common"),
+                plot.beds(chr, pos1, pos2, pos, user_defined_bed),
+                static.snp.plot,
+                gene.plot)),
+                ncol = 1, heights = c(rep(0.2,n_user_defined_tsv), 0.2 * n_user_defined_bw, 0.1 * n_user_defined_bed, 0.2, 0.1)
+            )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined MPRA ✅"))
+        }
         
     }
 } else {
     if (!is.null(user_defined_bed)) {
 
-        log.this.info(paste0("User defined bed to be plotted :\n", paste(user_defined_bed, collapse = "\n")))
+        message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", paste0("User defined bed to be plotted :\n", paste(user_defined_bed, collapse = "\n"))))
         n_user_defined_bed <- length(user_defined_bed)
         user.defined.plot <- wrap_plots(
             plot.beds(chr, pos1, pos2, pos, user_defined_bed),
@@ -155,20 +248,39 @@ if (!is.null(user_defined_bw)) {
             gene.plot,
             ncol = 1, heights = c(0.1 * n_user_defined_bed, 0.3, 0.2)
         )
-        log.this.info("User Defined Plots : Plotted user defined beds ✅") 
+        message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined beds ✅")) 
+
+        if (!is.null(user_defined_tsv_plots)) {
+            n_user_defined_tsv <- length(user_defined_tsv_plots)
+            user.defined.plot <- wrap_plots(
+                c(user_defined_tsv_plots, list(plot.beds(chr, pos1, pos2, pos, user_defined_bed),static.snp.plot, gene.plot)),
+                ncol = 1, heights = c(rep(0.2,n_user_defined_tsv), 0.1 * n_user_defined_bed, 0.2, 0.1)
+            )
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined MPRA ✅"))
+        }
         
     } else {
-        # Create an empty plot indicating no user-defined files
-        user.defined.plot <- ggplot() +
-            theme_minimal() +
-            annotate("text", x = 0.5, y = 0.5, label = "No bigwigs or bed files uploaded", size = 6, hjust = 0.5, vjust = 0.5) +
-            theme(
-                axis.title = element_blank(),
-                axis.text = element_blank(),
-                axis.ticks = element_blank(),
-                panel.grid = element_blank()
+
+        if (!is.null(user_defined_tsv_plots)) {
+            n_user_defined_tsv <- length(user_defined_tsv_plots)
+            user.defined.plot <- wrap_plots(
+                c(user_defined_tsv_plots, list(static.snp.plot, gene.plot)),
+                ncol = 1, heights = c(rep(0.2,n_user_defined_tsv), 0.2, 0.1)
             )
-        log.this.info("User Defined Plots : User has not defined beds or bigwigs. Plotted an empty plot. ⚠️")
+            message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : Plotted user defined MPRA ✅"))
+        } else {
+           # Create an empty plot indicating no user-defined files
+           user.defined.plot <- ggplot() +
+               theme_minimal() +
+               annotate("text", x = 0.5, y = 0.5, label = "No bigwigs, bed or MPRA data files uploaded", size = 6, hjust = 0.5, vjust = 0.5) +
+               theme(
+                   axis.title = element_blank(),
+                   axis.text = element_blank(),
+                   axis.ticks = element_blank(),
+                   panel.grid = element_blank()
+               )
+           message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "User Defined Plots : User has not defined beds, bigwigs or MPRA data. Plotted an empty plot. ⚠️"))
+        }
     }
 }
 
