d4f408c43b3fac711967db91d41c886b4c177926
vartikabisht6197 <vartika@annogen.bio>
2024-12-03 16:45:27 +0100

    The geen plot is transposed comoared to it's old position. This was because the number of genes are now in width and would not change and what is gonna change is only the height. So the height is now vairable and based on how many genes are in view.

--- modules/plot/gene.expression.R
+++ modules/plot/gene.expression.R
@@ -1,14 +1,15 @@
-
+ngenes <<- 3
 extra_genes = c("TBX18", "GATA4", "HNF4A")
 protien.coding.genes <- fread(file.path(dataDIR, "Homo.Sapiens.Gene.List.txt"), header = FALSE)$V1
 get.expression.plot = function(genes){
   genes = genes[genes %in% protien.coding.genes]
   if(length(genes)>0){
     genes = c(genes,extra_genes)
+    ngenes <<- length(genes)
     normcount = read.table(file.path(dataDIR, "Tissue_DevelopmentStages_CellLine_normcount.txt"))
     # Subset to genes of interest
     cnts = normcount[genes,]
-    g = pheatmap(t(cnts), cluster_rows = FALSE, show_rownames = TRUE, cluster_cols = FALSE, silent=TRUE)
+    g = pheatmap((cnts), cluster_rows = FALSE, show_rownames = TRUE, cluster_cols = FALSE, silent=TRUE)
   } else {
     g = ggplot() + 
       theme_minimal() +
