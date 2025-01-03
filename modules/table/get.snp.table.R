0acfe9a0c6c3e1df3bf02b62ec46b78f06598b91
vartikabisht6197 <vartika@annogen.bio>
2025-01-03 10:05:18 +0100

    Define column names properly and now SNP.info col name is Position not pos

--- modules/table/get.snp.table.R
+++ modules/table/get.snp.table.R
@@ -1,31 +1,38 @@
 
 highlight = NA
+SNP.info <- suredata
+col.order <- c(
+  "Chromosome", "Position",
+  "REF", "ALT",
+  "rsID", "Population allele frequency gnomAD 3.1.2",
+  "SuREX38", "SuREX57", "SuREX59", "SuREX67", "SuREX68", "SuREX86",
+  "REF allele coverage", "ALT allele coverage",
+  "REF allele mean expression", "ALT allele mean expression",
+  "p-value", "Description"
+)
+
 if( nrow(suredata) != 0 ){
-  SNP.info = suredata
   if(!is.na(pos)){
+    colnames(SNP.info) <- c(
+      "Chromosome", "Position",
+      "REF", "ALT",
+      "rsID", "Population allele frequency gnomAD 3.1.2",
+      "SuREX38", "SuREX57", "SuREX59",
+      "SuREX67", "SuREX68",
+      "SuREX86", "ALT allele coverage",
+      "REF allele coverage", "REF allele mean expression",
+      "ALT allele mean expression", "p-value", "Description"
+    )
+
+    SNP.info <- SNP.info[, col.order]
+
     # highlight the pos
-    highlight = which(SNP.info$POS == pos)
-  }
-  # colnames(SNP.info) <- c("Chromosome", "Position in human hg38",
-  # "Reference allele", "Alternate allele",
-  # "rsID", "Population allele frequency gnomAD 3.1.2",
-  #  "Genotype in SuRE57", "Genotype in SuRE59",
-  # "Genotype in SuRE67", "Genotype in SuRE68", "Genotype in SuRE86",
-  # "Alternate allele coverage", "Reference allele coverage",
-  # "Reference allele mean expression", "Alternate allele mean expression",
-  # "p-value")
-  
-  colnames(SNP.info) <- c("Chromosome", "Position in human hg38",
-                          "Reference allele", "Alternate allele",
-                          "rsID", "Population allele frequency gnomAD 3.1.2", 
-                          "SuREX38","SuREX57", "SuREX59",
-                          "SuREX67", "SuREX68", 
-                          "SuREX86", "Alternate allele coverage", 
-                          "Reference allele coverage", "Reference allele mean expression", 
-                          "Alternate allele mean expression", "p-value", "Description")
-  
+    highlight = which(SNP.info$Position == pos)
+  }  
   message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "SuRE Table : Functionally assessed variants in the region of interest printed in tabular format.  ✅"))
 } else {
-   message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "SuRE Table : No functionally assessed variants in the region of interest."))
-}
+  SNP.info <- data.frame(matrix(data = "", nrow = 1, ncol = length(col.order)))
+  colnames(SNP.info) <- col.order
 
+  message(paste(format(Sys.time(), "%d/%m/%Y %H:%M:%S"), ":", "SuRE Table : No functionally assessed variants in the region of interest."))
+}
